321746eaf372ac65e8947979650e6fea8fe74b9e
Michael Boroski <mjboroski@gmail.com>
2018-04-06 23:28:41 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,9 @@ def position_taken(array)
 end
 
 def position_taken?(board, index)
-  if board[index]==" " || board[index]==""
+  if board[index]==" " || board[index]=="" || board[index]==nil
     return false
+  elsif board[index]=="X" || board[index]=="O"
+    return true
   end
 end
